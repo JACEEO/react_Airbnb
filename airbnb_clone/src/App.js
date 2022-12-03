5f2df63fdf93fe443b590f0ab862bdc6dcd1d66c
JACEEO <juneeye92@gmail.com>
2022-12-03 23:20:13 +0900

    footer

--- airbnb_clone/src/App.js
+++ airbnb_clone/src/App.js
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import MainBody from "./components/MainBody";
 import Nav from "./components/Nav";
 import "./css/global.css";
@@ -10,6 +11,7 @@ function App() {
       <Banner />
       <Nav />
       <MainBody />
+      <Footer />
     </div>
   );
 }
---
+++ airbnb_clone/src/components/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div class="footer_wrap">
+        <div class="footer_box">
+          <div class="footer_map">
+            <span class="">© 2022 Airbnb, Inc.</span>
+            <span class="" aria-hidden="true">
+              ·
+            </span>
+            <a href="/terms/privacy_policy" class="">
+              개인정보 처리방침
+            </a>
+            <span class="" aria-hidden="true">
+              ·
+            </span>
+            <a href="/terms" class="">
+              이용약관
+            </a>
+            <span class="" aria-hidden="true">
+              ·
+            </span>
+            <a href="/sitemaps/v2" class="">
+              사이트맵
+            </a>
+            <span class="" aria-hidden="true">
+              ·
+            </span>
+            <a
+              target="_blank"
+              href="/home/updated_cancellation_policies?korean_strict_policy=true#strict"
+              class=""
+            >
+              한국의 변경된 환불 정책
+            </a>
+            <span class="" aria-hidden="true">
+              ·
+            </span>
+            <a target="_blank" href="/about/company-details" class="">
+              회사 세부정보
+            </a>
+          </div>
+
+          <div class="footer_mid"></div>
+
+          <div></div>
+        </div>
+
+        <div class="footer_info">
+          <div class="footer_info_detail">
+            웹사이트 제공자: Airbnb Ireland UC, private unlimited company, 8
+            Hanover Quay Dublin 2, D02 DP23 Ireland | 이사: Dermot Clarke,
+            Killian Pattwell, Andrea Finnegan | VAT 번호: IE9827384L | 사업자
+            등록 번호: IE 511825 | 연락처: terms@airbnb.com, 웹사이트,
+            080-822-0230 | 호스팅 서비스 제공업체: 아마존 웹서비스 |
+            에어비앤비는 통신판매 중개자로 에어비앤비 플랫폼을 통하여 게스트와
+            호스트 사이에 이루어지는 통신판매의 당사자가 아닙니다. 에어비앤비
+            플랫폼을 통하여 예약된 숙소, 체험, 호스트 서비스에 관한 의무와
+            책임은 해당 서비스를 제공하는 호스트에게 있습니다.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
