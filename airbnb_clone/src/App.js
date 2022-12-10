a581ed250cbf308cb7559ff240cdcdc92b262eaa
JACEEO <juneeye92@gmail.com>
2022-12-10 16:29:13 +0900

    notfound page

--- airbnb_clone/src/App.js
+++ airbnb_clone/src/App.js
@@ -5,12 +5,14 @@ import Home from "./pages/Home";
 import "./css/global.css";
 import "./css/main.css";
 import "./css/detail.css";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 }
