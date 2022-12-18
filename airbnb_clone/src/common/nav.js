8901b990daff9e0be0cc8031b3b2ddab3ae149fa
JACEEO <juneeye92@gmail.com>
2022-12-18 22:35:38 +0900

    nav styled js

---
+++ airbnb_clone/src/common/nav.js
@@ -0,0 +1,180 @@
+import styled from "styled-components";
+
+export const NavBar = styled.div`
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+
+  max-width: 1470px;
+
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
+  background-color: #ffffff;
+  height: ${(props) => (props.value ? "100%" : "100px")};
+
+  padding: 0 80px;
+  border-bottom: 1px solid #dddddd;
+
+  transform: translateY(${(props) => (props.value ? "0" : "70px")});
+  transition: transform 250ms ease;
+`;
+
+export const NavLink = styled.a`
+  cursor: pointer;
+  color: #ff385c;
+
+  @media screen and (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SearchWrap = styled.div`
+  padding: 0 24px;
+  flex: 0 1 auto;
+  width: 460px;
+`;
+
+export const NavSearch = styled.div`
+  max-width: 1120px;
+
+  text-align: left;
+  background-color: #ffffff;
+  border: 1px solid #dddddd;
+  border-radius: 40px;
+  box-shadow: 0 1px 2px rgb(0 0 0 / 8%), 0 4px 12px rgb(0 0 0 / 5%);
+
+  align-items: center;
+  display: inline-flex;
+
+  visibility: ${(props) => (props.value ? "visible" : "hidden")};
+
+  padding: 0 10px;
+`;
+
+export const SearchBox = styled.div`
+  position: absolute;
+  top: 0;
+  visibility: ${(props) => (props.value ? "hidden" : "visible")};
+
+  border: 1px solid #dddddd;
+  border-radius: 40px;
+
+  width: 370px;
+  height: 60px;
+
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const SearchInput = styled.input`
+  border: none;
+  height: 30px;
+  width: 300px;
+  position: absolute;
+  top: 13px;
+  left: 22px;
+
+  font-size: large;
+  color: gray;
+
+  z-index: 100;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+export const SearchBoxBtn = styled.button`
+  background-color: #ffffff;
+  cursor: pointer;
+
+  border-radius: 40px;
+
+  margin-right: 10px;
+`;
+
+export const SearchUl = styled.ul`
+  visibility: ${(props) => (props.value ? "hidden" : "visible")};
+
+  margin-top: ${(props) => (props.value ? "0" : "10px")};
+  width: 370px;
+
+  z-index: 1000;
+
+  height: ${(props) => (props.value ? "0" : "10px")};
+`;
+
+export const SearchLi = styled.li`
+  height: 30px;
+
+  border: 1px solid #dddddd;
+  border-radius: 5px;
+
+  background-color: #ffffff;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+`;
+
+export const Profile = styled.div`
+  width: 320px;
+  height: 80px;
+
+  display: flex;
+  justify-content: space-between;
+
+  align-items: center;
+
+  @media screen and (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+export const ProfileA = styled.a`
+  margin-right: 8px;
+  font-size: 0.9rem;
+  font-weight: 600;
+
+  @media screen and (max-width: 1240px) {
+    font-size: 0.5rem;
+  }
+`;
+
+export const ProfileDetail = styled.div`
+  border: 1px solid #dddddd;
+  border-radius: 50px;
+  width: 77px;
+  height: 42px;
+  align-items: center;
+  justify-content: space-around;
+  display: flex;
+
+  position: relative;
+
+  cursor: pointer;
+`;
+
+export const ProfileLogin = styled.div`
+  width: 250px;
+  height: 250px;
+
+  box-shadow: 1px 1px 5px 1px #dddddd;
+
+  position: absolute;
+
+  border: none;
+  border-radius: 10px;
+  top: 50px;
+  right: 0;
+
+  background-color: #ffffff;
+
+  display: ${(props) => (props.value ? "none" : "flex")};
+  justify-content: space-around;
+  flex-direction: column;
+`;
--- airbnb_clone/src/components/Nav.jsx
+++ airbnb_clone/src/components/Nav.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
-import styled from "styled-components";
+import {
+  NavBar,
+  NavSearch,
+  Profile,
+  ProfileA,
+  ProfileDetail,
+  ProfileLogin,
+  SearchBox,
+  SearchBoxBtn,
+  SearchInput,
+  SearchLi,
+  SearchUl,
+  SearchWrap,
+} from "../common/nav";
 
 import dummy from "../hook/data.json";
 import { onTrue } from "../recoil/airplane";
@@ -219,182 +232,3 @@ export default function Nav() {
     </NavBar>
   );
 }
-
-const NavBar = styled.div`
-  justify-content: space-between;
-  align-items: center;
-  display: flex;
-
-  max-width: 1470px;
-
-  position: sticky;
-  top: 0;
-  z-index: 10;
-
-  background-color: #ffffff;
-  height: ${(props) => (props.value ? "100%" : "100px")};
-
-  padding: 0 80px;
-  border-bottom: 1px solid #dddddd;
-
-  transform: translateY(${(props) => (props.value ? "0" : "70px")});
-  transition: transform 250ms ease;
-`;
-
-const NavLink = styled.a`
-  cursor: pointer;
-  color: #ff385c;
-
-  @media screen and (max-width: 700px) {
-    display: none;
-  }
-`;
-
-const SearchWrap = styled.div`
-  padding: 0 24px;
-  flex: 0 1 auto;
-  width: 460px;
-`;
-
-const NavSearch = styled.div`
-  max-width: 1120px;
-
-  text-align: left;
-  background-color: #ffffff;
-  border: 1px solid #dddddd;
-  border-radius: 40px;
-  box-shadow: 0 1px 2px rgb(0 0 0 / 8%), 0 4px 12px rgb(0 0 0 / 5%);
-
-  align-items: center;
-  display: inline-flex;
-
-  visibility: ${(props) => (props.value ? "visible" : "hidden")};
-
-  padding: 0 10px;
-`;
-
-const SearchBox = styled.div`
-  position: absolute;
-  top: 0;
-  visibility: ${(props) => (props.value ? "hidden" : "visible")};
-
-  border: 1px solid #dddddd;
-  border-radius: 40px;
-
-  width: 370px;
-  height: 60px;
-
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const SearchInput = styled.input`
-  border: none;
-  height: 30px;
-  width: 300px;
-  position: absolute;
-  top: 13px;
-  left: 22px;
-
-  font-size: large;
-  color: gray;
-
-  z-index: 100;
-
-  :focus {
-    outline: none;
-  }
-`;
-
-const SearchBoxBtn = styled.button`
-  background-color: #ffffff;
-  cursor: pointer;
-
-  border-radius: 40px;
-
-  margin-right: 10px;
-`;
-
-const SearchUl = styled.ul`
-  visibility: ${(props) => (props.value ? "hidden" : "visible")};
-
-  margin-top: ${(props) => (props.value ? "0" : "10px")};
-  width: 370px;
-
-  z-index: 1000;
-
-  height: ${(props) => (props.value ? "0" : "10px")};
-`;
-
-const SearchLi = styled.li`
-  height: 30px;
-
-  border: 1px solid #dddddd;
-  border-radius: 5px;
-
-  background-color: #ffffff;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  cursor: pointer;
-`;
-
-const Profile = styled.div`
-  width: 320px;
-  height: 80px;
-
-  display: flex;
-  justify-content: space-between;
-
-  align-items: center;
-
-  @media screen and (max-width: 1000px) {
-    display: none;
-  }
-`;
-
-const ProfileA = styled.a`
-  margin-right: 8px;
-  font-size: 0.9rem;
-  font-weight: 600;
-
-  @media screen and (max-width: 1240px) {
-    font-size: 0.5rem;
-  }
-`;
-
-const ProfileDetail = styled.div`
-  border: 1px solid #dddddd;
-  border-radius: 50px;
-  width: 77px;
-  height: 42px;
-  align-items: center;
-  justify-content: space-around;
-  display: flex;
-
-  position: relative;
-
-  cursor: pointer;
-`;
-
-const ProfileLogin = styled.div`
-  width: 250px;
-  height: 250px;
-
-  box-shadow: 1px 1px 5px 1px #dddddd;
-
-  position: absolute;
-
-  border: none;
-  border-radius: 10px;
-  top: 50px;
-  right: 0;
-
-  background-color: #ffffff;
-
-  display: ${(props) => (props.value ? "none" : "flex")};
-  justify-content: space-around;
-  flex-direction: column;
-`;
